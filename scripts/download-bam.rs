6b0d282bb746d4fcfb75c0c35e961bede793d64a
Alexis Praga <apraga@chu-besancon.fr>
2024-02-11 17:00:09 +0100

    Corrected bam download : idt has a different path

--- scripts/download-bam.rs
+++ scripts/download-bam.rs
@@ -1,9 +1,23 @@
 use std::fs;
 use std::process::Command;
+use std::io::{self, Write};
+use std::path::PathBuf;
 
-fn generate_url(sequencer: &str, capture: &str, coverage: &str, patient: &str) -> String {
+// Return the url down to the directory level for a sets of patients
+fn root_url(sequencer: &str, capture: &str, coverage: &str) -> String {
     let root = "https://storage.googleapis.com/brain-genomics-public/research/sequencing/grch38/bam";
-    format!("{root}/{sequencer}/wes_{capture}/{coverage}/{patient}.{sequencer}.wes-{capture}.{coverage}.dedup.grch38.bam")
+    format!("{root}/{sequencer}/wes_{capture}/{coverage}")
+}
+
+// Retur nthe bam filename
+fn bam_file(sequencer: &str, capture: &str, coverage: &str, patient: &str) -> String {
+    // Underscore in filename for idt
+    if capture == "idt" {
+        format!("{patient}.{sequencer}.wes_{capture}.{coverage}.dedup.bam")
+    }
+    else {
+        format!("{patient}.{sequencer}.wes-{capture}.{coverage}.dedup.grch38.bam")
+    }
 }
 
 fn download(sequencer: &str, capture: &str, coverage: &str) {
@@ -11,18 +25,28 @@ fn download(sequencer: &str, capture: &str, coverage: &str) {
     fs::create_dir_all(&outdir).expect("Failed to create output directory");
 
     let patients: Vec<_> = (1..=7).map(|x| format!("HG00{x}")).collect();
-    let urls: Vec<_> = patients.iter()
-                               .map(|patient| generate_url(sequencer, capture, coverage, patient))
-                               .collect();
-
-    for url in urls {
-        println!("Downloading {} {} : {}", sequencer, capture, url);
-        Command::new("sdf")
-                .arg("get")
-                .arg(&url)
-                .output()
-                .expect("Failed to execute sdf command");
+    for patient in patients {
+        let dir = root_url(&sequencer, &capture, &coverage);
+        let bam = bam_file(&sequencer, &capture, &coverage, &patient);
+        let url = format!("{dir}/{bam}");
+        if PathBuf::from(&outdir).join(&bam).exists() {
+            println!("{bam} already exists, skipping");
+        }
+        else {
+            download_url(&url, &outdir)
+        }
     }
+}
+
+fn download_url(url: &str, outdir: &str) {
+    println!("Downloading {url} into {outdir}");
+    let output = Command::new("sdf")
+                    .arg("get")
+                    .arg(&url)
+                    .current_dir(&outdir)
+                    .output()
+                    .expect("Failed to execute sdf command");
+    io::stderr().write_all(&output.stderr).unwrap();
 }
 
 fn main() {
