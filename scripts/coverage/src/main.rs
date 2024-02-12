f88a7dc87951beb98f291a58b4dedf7a1fbd6e92
Alexis Praga <apraga@chu-besancon.fr>
2024-02-12 23:07:47 +0100

    Better tool to compute coverage

--- scripts/coverage/src/main.rs
+++ scripts/coverage/src/main.rs
@@ -1,44 +1,76 @@
-use std::process::Command;
+use clap::Parser;
 use glob::glob;
+use std::fs;
 use std::path::{Path, PathBuf};
-use std::str;
+use std::process::Command;
+// use std::io::{Error, ErroKind};
+use std::error::Error;
 
-fn capture(stem: &str) -> PathBuf {
-    if stem.contains("idt") {
-        return PathBuf::from("idt_capture.grch38.bed");
-    }
-    else if stem.contains("truseq") {
-        return PathBuf::from("truseq-dna-exome-targeted-regions-manifest-v1-2-hg38.bed");
-    }
-    else {
+fn capture(path: &Path) -> Result<PathBuf, Box<dyn Error>> {
+    let path_str = path.to_str().ok_or("Path is not valid UTF-8")?;
+    let capt_base = PathBuf::from("../../baid2020/bed");
+
+    let capt_file = if path_str.contains("idt") {
+        "idt_capture.grch38.bed"
+    } else if path_str.contains("truseq") {
+        "truseq-dna-exome-targeted-regions-manifest-v1-2-hg38.bed"
+    } else {
         println!("Agilent by default");
-        return PathBuf::from("agilent.targets.grch38.bed");
-    }
+        "agilent.targets.grch38.bed"
+    };
 
+    Ok(capt_base.join(capt_file))
 }
-fn mosdepth(path: PathBuf) {
-    let stem = path.file_stem().expect("Fails to extract file stem").to_str().unwrap();
-    // TODO mkdir this directory
-    let out = String::from("baid2020/") + stem;
-    let dir = path.parent().unwrap();
-    let fasta = "../../genome/GCA_000001405.15_GRCh38_full_analysis_set.fna.gz";
-    let capt_path = PathBuf::from("../../baid2020/bed").join(capture(stem));
-    let capt = capt_path.to_str().unwrap() ;
-    println!("{} : {}", stem, capt);
+
+fn mosdepth(path: PathBuf, outdir: &Path) -> Result<(), Box<dyn Error>> {
+    let stem = path.file_stem().ok_or("Fails to extract file stem")?.to_str().ok_or("Stem is not valid UTF-8")?;
+    let prefix = outdir.join(stem);
+    let fasta = PathBuf::from("../../genome/GCA_000001405.15_GRCh38_full_analysis_set.fna.gz");
+    let capt = capture(&path)?;
+    let capt_str = capt.to_str().ok_or("Capture path is not valid UTF-8")?;
+
+    assert!(fasta.exists());
+    assert!(capt.exists());
+
+    println!("{:?} : {}", stem, capt.display());
     let output = Command::new("mosdepth")
-        .args(["-x", "--threads", "6", "--by",  &capt, "--fasta", fasta])
-        .args([&out, path.to_str().unwrap()])
-        .output
-        ().expect("Fails to run command");
-    println!("{}", str::from_utf8(&output.stdout).unwrap());
-    println!("{}", str::from_utf8(&output.stderr).unwrap());
+        .args(["-x", "--threads", "6", "--by", capt_str, "--fasta", fasta.to_str().unwrap()])
+        .args([prefix.to_str().unwrap(), path.to_str().unwrap()])
+        .output()?;
+
+    println!("{}", String::from_utf8(output.stdout)?);
+    println!("{}", String::from_utf8(output.stderr)?);
+    Ok(())
 }
 
-fn main() {
-    for entry in glob("../../baid2020/grch38/bam/**/*.bam").expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => mosdepth(path),
-            Err(e) => println!("{:?}", e),
-        }
+fn coverage(indir: &str, outdir: &Path) -> Result<(), Box<dyn Error>> {
+    let expr = format!("{}/**/HG*.bam", indir);
+    for entry in glob(&expr)? {
+        let path = entry?;
+        mosdepth(path, outdir)?;
     }
+    Ok(())
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Directory searched for bam files (recursive)
+    #[arg(short, long)]
+    dir: String,
+
+    /// Output directory
+    #[arg(short, long)]
+    out: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let outdir = PathBuf::from(&args.out);
+    fs::create_dir_all(&outdir)?;
+
+    coverage(&args.dir, &outdir)?;
+
+    Ok(())
 }
