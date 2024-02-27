d8e9c4786b178319bc7d231a811b84ba01ebd20b
Alexis Praga <alexis.praga@proton.me>
2024-02-27 22:45:44 +0100

    Packaging varben with nix
    Patch with python3 support and a bug correction about non-existing alignemet pair

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-23.11";
+    };
+
+    description = "Exome validator";
+
+    outputs = { self, nixpkgs }:
+        let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            packages.${system} = {
+                varben = pkgs.callPackage pkgs/varben.nix {};
+            };
+        };
+}
