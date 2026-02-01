65cd2e3cb7c9a2c01460047389e076452e9f34ef
Alexis Praga <alexis@praga.dev>
2026-02-01 22:58:45 +0100

    Rust as a glue : CLI setup
    Read GIAB patient from TOML

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+use toml;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    real: Option<RealConfig>,
+    silico: Option<SilicoConfig>,
+}
+
+#[derive(Deserialize, Debug)]
+enum Patient {
+    HG001,
+    HG002,
+    HG003,
+    HG004,
+    HG005,
+}
+
+#[derive(Deserialize, Debug)]
+struct SilicoConfig {
+    patients: Vec<Patient>,
+}
+
+#[derive(Deserialize, Debug)]
+struct RealConfig {
+    patients: Vec<Patient>,
+}
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Sets a custom config file (TOML)
+    #[arg(short, long, value_name = "FILE")]
+    config: PathBuf,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate samplesheet for all use cases
+    Samplesheet {},
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    println!("Value for config: {}", cli.config.display());
+
+    let content = std::fs::read_to_string(cli.config).unwrap();
+    let config: Config = toml::from_str(&content).unwrap();
+    println!("{:?}", config);
+    if let Some(real) = config.real {
+        println!("Real patients: {:?}", real.patients);
+    } else {
+        println!("No real patients...");
+    }
+
+    if let Some(silico) = config.silico {
+        println!("Silico patients: {:?}", silico.patients);
+    } else {
+        println!("No silico patients...");
+    }
+    // if let Some(config_path) = cli.config.as_deref() {
+    // println!("Value for config: {}", config_path.display());
+    // }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Samplesheet {}) => {}
+        None => {}
+    }
+
+    // Continued program logic goes here...
+}
